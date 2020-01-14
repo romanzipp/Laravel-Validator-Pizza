93522df4b88fa1775ec8a77eb4c0b5a2f56ecf55
Thomas Petracco <git@mail.tompec.com>
2020-01-14 12:36:47 +0800

    Create first test

---
+++ tests/TestCase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace romanzipp\ValidatorPizza\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('migrate')->run();
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('validator-pizza.store_checks', false);
+        $app['config']->set('validator-pizza.cache_checks', false);
+    }
+
+    /**
+     * Get package providers.  At a minimum this is the package being tested, but also
+     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
+     * In a normal app environment these would be added to the 'providers' array in
+     * the config/app.php file.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \romanzipp\ValidatorPizza\Providers\ValidatorPizzaProvider::class,
+        ];
+    }
+}
